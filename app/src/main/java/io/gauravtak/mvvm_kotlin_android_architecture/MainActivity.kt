e165e9f1188ec5b71a8d4da8bc90531983adf18a
Gaurav Tak <gaurav.tak@roro.io>
2024-01-01 17:18:02 +0530

    other files also added

---
+++ app/src/main/java/io/gauravtak/mvvm_kotlin_android_architecture/MainActivity.kt
@@ -0,0 +1,11 @@
+package io.gauravtak.mvvm_kotlin_android_architecture
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
